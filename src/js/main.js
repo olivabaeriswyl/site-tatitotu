4495a7779fe0c9e2b3014f7f8e3a448c62c6cc4f
olivabaeriswyl <122967424+olivabaeriswyl@users.noreply.github.com>
2024-04-22 15:33:36 +0200

    bouton préréglage

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,32 @@
 var body = document.body;
+var creativeInput = document.querySelector("#creative");
 var soberInput = document.querySelector("#sobre");
+
 soberInput.addEventListener("change", function () {
   body.classList = [];
   body.classList.add("sober");
 });
 
-var creativeInput = document.querySelector("#creative");
 creativeInput.addEventListener("change", function () {
   body.classList = [];
   body.classList.add("creative");
 });
+
+var checkAllTrigger = document.querySelector(".check-all");
+var checkboxes = document.querySelectorAll(".checkbox");
+
+var checkAll = function () {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = true;
+  });
+};
+
+checkAllTrigger.addEventListener("click", checkAll);
+
+var checkAll = function () {
+  checkboxes.forEach((checkbox) => {
+    checkbox.checked = false;
+  });
+};
+
+checkAllTrigger.addEventListener("click", checkAll);
